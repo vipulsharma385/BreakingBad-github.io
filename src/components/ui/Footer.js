38f393c85ca06fc14cdc8fd0fe80944cd86b5b96
vipulsharma385 <77441682+vipulsharma385@users.noreply.github.com>
2022-02-11 12:35:49 +0530

    project

---
+++ src/components/ui/Footer.js
@@ -0,0 +1,13 @@
+
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='center'>
+      <br/>
+        <span>Created By<a href='#'> Vipul Sharma</a></span>
+    </footer>
+  )
+}
+
+export default Footer
